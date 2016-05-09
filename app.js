07621f6181cd4031f4430649c952c71af3a8b2d9
Quentin Gérard <qgerard.gerard@gmail.com>
2016-05-09 12:54:38 +0200

    added https package

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cons = require('consolidate');
+var nodemailer = require('nodemailer');
+var https = require('https');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,6 +17,7 @@ var app = express();
 // app.engine('html', cons.swig);
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
