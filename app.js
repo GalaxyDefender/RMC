b9132e38141f3825b4277b61f0457f9210f98ec9
Quentin Gérard <qgerard.gerard@gmail.com>
2016-08-22 12:34:35 +0200

    added my own node.js designs for the site

--- app.js
+++ app.js
@@ -1,33 +1,124 @@
-/**
- * Created by shane on 3/9/15.
- */
-//var CONFIG = require('./config/config.js');
-//var CONFIG;
-//var SETINGS;
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cons = require('consolidate');
+var nodemailer = require('nodemailer');
+var https = require('https');
 
-global.__base = __dirname + '/';
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-var Q               = require('q');
-var express         = require('express'); 			// call express
-var cors	       	= require('cors');				// call cors
-var bodyParser  	= require('body-parser');		// call body-parser
-var http            = require('http');
-var path            = require('path');
+var app = express();
 
-var app	= express();
-app.use(cors());
-// Use parsing to extract JSON object from POST requests
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json( {limit: '50mb'} ));
-
-app.use(express.static(path.join(__dirname, 'app')));
-
-var website = require('./app/routes/website');
-app.use('/',website);
-
-var server = http.createServer(app);
-server.listen(8080);
+// view engine setup
+// app.engine('html', cons.swig);
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/', function(req,res){
+  var mailOpts, smtpTrans;
+
+  var googleResponse = "";
+  var SECRET = "6LfoDx8TAAAAAJEfLEqpK88UWY1yJb6NMwfkSll7"; //secret key from google catcha
+
+  // Display's message when user didn't use CAPTCHA
+  if(req.body["g-recaptcha-response"] === undefined || req.body["g-recaptcha-response"] === '' || req.body["g-recaptcha-response"] === null){
+    return res.render('index', {CAPTCHA: 'Please select CAPTCHA'});
+  }
+
+  // This code figures out the CAPTCHA
+  var httpsReq = https.request('https://www.google.com/recaptcha/api/siteverify' + '?secret=' + SECRET + '&response=' + req.body["g-recaptcha-response"], function(httpsRes){
+    httpsRes.on("data", function(chunk){
+      googleResponse += chunk;
+    });
+    httpsRes.on("end", function(){
+      res.render('sent');
+    });
+  });
+
+  httpsReq.on("error", function(err){
+    res.send("Error:" + JSON.stringify(err));
+  });
+
+  httpsReq.end();
+
+  // mail options settings
+  mailOpts = {
+    from: 'qgerard.gerard@gmail.com',
+    to: 'quentin@realtelematics.co.za',
+    subject: 'Website contact - ' + req.body.inputSubject,
+    html: '<h1>' + req.body.inputCompany + '</h1><p>' + req.body.inputText + '</p><br><p>Name: ' + req.body.inputName + ' ' + req.body.inputSurname + '</p><p>Email address: ' + req.body.inputEmail + '</p><p>Contact number: ' + req.body.inputPhone + '</p><p>Region: ' + req.body.inputRegion + '</p>' 
+  };
+
+  // SMTP transporter
+  smtpTrans = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "qgerard.gerard@gmail.com",
+        pass: "mwcciqcglhnukibf"
+    }
+  });
+
+  // Sending the email
+  smtpTrans.sendMail(mailOpts, function(error, message){
+    if(error){
+          res.render('/');
+          console.log(error);
+      }else{
+          console.log(req.body.inputName + ' <' + req.body.inputEmail + '>');
+          res.render('sent');
+          console.log("Message sent");
+        }
+
+  });
+    smtpTrans.close();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
+module.exports = app;
---
+++ public/js/custom.js
@@ -0,0 +1,2 @@
+var x = document.getElementById("inputName")[0].className;
+document.getElementById("demo").innerHTML = document.getElementById("inputName")[0].className;
